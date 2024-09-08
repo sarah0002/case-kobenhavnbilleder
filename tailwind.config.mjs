9639d6ed5c0d221f9ec17d3bd1d9c95c48234e45
Sarah Albaek <saal0002@stud.kea.dk>
2024-09-08 13:25:25 +0200

    sidste commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: { 
+            colors: {
+            'baggrund': '#F8F8F8',
+            'farve1': '#252422',
+            'farve2': '#D66445',
+            'farve3': '#E7E6E3',
+
+
+          },},
 	},
 	plugins: [
         require('@tailwindcss/typography'),
